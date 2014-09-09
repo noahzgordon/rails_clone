d784ef210071f4ca870ba5efc8aa90114af51d72
Noah Gordon <tortuga.ng@gmail.com>
2014-09-08 22:45:26 -0400

    Rename active_record files.

---
+++ skeleton/lib/active_record/core.rb
@@ -0,0 +1,4 @@
+require_relative 'sql_object'
+require_relative 'searchable'
+require_relative 'associatable'
+require_relative 'relation'
--- skeleton/lib/active_record_lite/core.rb
+++
@@ -1,5 +0,0 @@
-require_relative '01_sql_object'
-require_relative '02_searchable'
-require_relative '03_associatable'
-require_relative '04_associatable2'
-require_relative '05_relation'
